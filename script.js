2a16eff08982f600a4d5f43d0b2a969468f77c89
Jake Hunter <jake7hunter@hmail.com>
2021-12-17 03:29:14 -0500

    Added comments to make functions and variables usage more clear

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
+//Variables to access thumbnails and hero image
 const thumb = document.querySelectorAll(`.thumbnail`)
-
 const large = document.querySelector(`#large-img`)
 
-// const setHeroImage = (thumb) => {
-//     const url = thumb.getAttribute(`src`)
-//     console.log(url)
-//     large.setAttribute(`src`, url)
-
-// }
-
-// thumb.addEventListener(`click`, function(event){
-//     const thumbNail = event.target
-//     const url = thumbNail.getAttribute(`src`)
-//     console.log(url)
-//     large.setAttribute(`src`, url)
-// })
-
+//Function to get images attributes and assign them to hero image
 const thumbClick = function(event){
     const thumbNail = event.target
     const url = thumbNail.getAttribute(`src`)
     console.log(url)
     large.setAttribute(`src`, url)
-
 }
 
+//Allows each thumbnail to be individually selected
 thumb.forEach(function(eleThumb){
     eleThumb.addEventListener(`click`, thumbClick)
 })
